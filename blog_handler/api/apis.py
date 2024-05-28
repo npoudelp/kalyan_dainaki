1ac2f8ffc1a2494ac4bea615b1723e7d36bf379f
npoudelp <npoudelp@gmail.com>
2024-05-28 20:29:51 +0545

    api for blog will be here

---
+++ blog_handler/api/apis.py
@@ -0,0 +1,8 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+class Home(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        return Response("Hello, World!")
