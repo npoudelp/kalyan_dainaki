ade84ccab8487b4a9871e25b2af9c9757f539187
npoudelp <npoudelp@gmail.com>
2024-05-28 21:27:55 +0545

    catagory model setup

--- blog_handler/api/models.py
+++ blog_handler/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Catagory(models.Model):
+    CatagoryName = models.CharField(max_length=25)
+    
+    def __str__(self):
+        return self.catagory_name
