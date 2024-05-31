6dc267d85848f9e3de1c5ceede41ae88daf8b60e
npoudelp <npoudelp@gmail.com>
2024-05-31 11:23:39 +0545

    user model serializers made

--- blog_handler/api/serializers.py
+++ blog_handler/api/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
 from .models import Catagory
+from django.contrib.auth.models import User
+
 
 class CatagorySerializers(serializers.ModelSerializer):
     class Meta:
         model = Catagory
+        fields = '__all__'
+
+
+class UserSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = User
         fields = '__all__'
