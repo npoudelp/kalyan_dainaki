d87dfc705378b8a5240cc17883aa99346e9d0530
npoudelp <npoudelp@gmail.com>
2024-05-31 13:50:04 +0545

    urlpattern managed

--- blog_handler/blog_handler/urls.py
+++ blog_handler/blog_handler/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from .views import index
 from api import urls as api_urls
+from django.conf import settings
+from django.conf.urls.static import static
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -35,3 +37,4 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
