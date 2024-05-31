08b42e2f457f6d90da4ecd7bb775020057aaa30f
npoudelp <npoudelp@gmail.com>
2024-05-31 13:48:44 +0545

    blog model added

--- blog_handler/api/models.py
+++ blog_handler/api/models.py
@@ -1,9 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Catagory(models.Model):
-    CatagoryName = models.CharField(max_length=25)
+    catagoryName = models.CharField(max_length=25)
     
     def __str__(self):
-        return self.catagory_name
+        return self.catagoryName
+    
+
+
+class Blogs(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    catagory = models.ForeignKey(Catagory, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    blogDate = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='blog_thumbnails/')
+    
+    def __str__(self):
+        return self.title
