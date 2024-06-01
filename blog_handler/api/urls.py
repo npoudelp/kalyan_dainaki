273926f71906979e273bf2937980f14231817277
npoudelp <npoudelp@gmail.com>
2024-06-01 07:57:20 +0545

    url name changed to make it more obvious

--- blog_handler/api/urls.py
+++ blog_handler/api/urls.py
@@ -3,7 +3,7 @@ from . import apis
 
 urlpatterns = [
     path('', apis.Home.as_view(), name='home'),
-    path('catagory/', apis.CatagoryList.as_view(), name='catagory'),
+    path('manage_catagory/', apis.CatagoryManager.as_view(), name='catagory'),
     path('users/', apis.RegisterUsers.as_view(), name='users'),
     path('manage_blogs/', apis.BlogManager.as_view(), name='blog_manager'),
     path('blogs/', apis.GetAllBlogs.as_view(), name='get_blogs'),
