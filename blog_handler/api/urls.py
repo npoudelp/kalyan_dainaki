97ba9b462fd1c63d7f8c63fa79e1fc7c3cc7927f
npoudelp <npoudelp@gmail.com>
2024-06-02 15:49:31 +0545

    url updated to clear mismatch

--- blog_handler/api/urls.py
+++ blog_handler/api/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('users/', apis.RegisterUsers.as_view(), name='users'),
     path('manage_blogs/', apis.BlogManager.as_view(), name='blog_manager'),
     path('blogs/', apis.GetAllBlogs.as_view(), name='get_blogs'),
-    path('blogs/<int:id>/', apis.GetBlogById.as_view(), name='get_blog_by_id'),
-    path('blogs/<str:key>/', apis.SearchBlog.as_view(), name='search_blog'),
-    path('blogs/<int:id>/add_view/', apis.UpdateView.as_view(), name='update_view'),
+    path('blogs/get/<int:id>/', apis.GetBlogById.as_view(), name='get_blog_by_id'),
+    path('blogs/find/<str:key>/', apis.SearchBlog.as_view(), name='search_blog'),
+    path('blogs/add_view/<int:id>', apis.UpdateView.as_view(), name='update_view'),
 ]
